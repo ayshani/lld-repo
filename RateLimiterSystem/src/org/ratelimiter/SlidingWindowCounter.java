986b4a367f2ef4a26c158a04e7c166e16ab026f1
conjuring420 <conjuring420@outlook.com>
2022-08-06 15:38:49 +0530

    commit

---
+++ RateLimiterSystem/src/org/ratelimiter/SlidingWindowCounter.java
@@ -0,0 +1,39 @@
+package org.ratelimiter;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SlidingWindowCounter extends RateLimiter{
+
+    private final ConcurrentMap<Long, AtomicInteger> windows ;
+    public SlidingWindowCounter(int maxRequestPerSec) {
+        super(maxRequestPerSec);
+        windows = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public boolean allowRequest() {
+        long currentTime = System.currentTimeMillis();
+        long currentWindowKey = currentTime/1000;
+
+        windows.putIfAbsent(currentWindowKey,new AtomicInteger(0));
+        long previousWindowKey = currentWindowKey -1;
+
+        AtomicInteger previousWindowCount = windows.get(previousWindowKey);
+
+        if(previousWindowCount == null){
+            return windows.get(currentWindowKey).getAndIncrement() <= MAX_TOKEN;
+        }
+
+        long currentWindowOverlapTime = currentTime - (currentWindowKey*1000);
+
+        double previousWindowOverlapPercentage = (1000 - currentWindowOverlapTime) /1000.0;
+
+        long requestCountInCurrentSlidingWindow = (long)(previousWindowCount.get()* previousWindowOverlapPercentage
+                                                    + windows.get(currentWindowKey).getAndIncrement());
+
+        return requestCountInCurrentSlidingWindow <= MAX_TOKEN;
+
+    }
+}
